b76a692c9cac5d965d113f4243a6f4da2bb4f836
Tenn1518 <Tenn1518@users.noreply.github.com>
2021-12-03 10:46:43 -0500

    fixup! Emacs: Optimize startup by correctly deferring loading

--- config/emacs/early-init.el
+++ config/emacs/early-init.el
@@ -1,2 +1,16 @@
-;; Stops emacs's default package system from initializing
+;; disable garbage collection until post-init
+(setq gc-cons-threshold-original gc-cons-threshold
+      gc-cons-threshold (* 1024 1024 100))
+
+;; Stop Emacs's default package system from initializing
 (setq package-enable-at-startup nil)
+
+;; Stop text-mode from eager loading some packages
+(setq initial-major-mode 'fundamental-mode)
+
+;; Stop Emacs from mangling the frame on startup
+(setq frame-inhibit-implied-resize t)
+
+;; Stop Emacs from checking each file for major mode during startup
+(defvar t/file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
