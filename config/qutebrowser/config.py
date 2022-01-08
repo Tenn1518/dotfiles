9b2657c9b02e158245a508d3b2bd1d2157173111
Tenn1518 <Tenn1518@users.noreply.github.com>
2022-01-08 18:02:45 -0500

    Add qutebrowser configuration

---
+++ config/qutebrowser/config.py
@@ -0,0 +1,10 @@
+# qutebrowser configuration
+
+config.load_autoconfig(False)
+# Install python-adblock
+c.content.blocking.method = "both"
+c.fonts.default_size = "12pt"
+
+# Keybindings
+config.bind('[b', 'back')
+config.bind(']b', 'forward')
