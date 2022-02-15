d0cb2c865b09ce5835859a2866c154f6441ea1dc
Tenn1518 <Tenn1518@users.noreply.github.com>
2022-02-14 21:57:17 -0500

    Xmonad: Add layouts, spacing, and EWMH compliance

--- config/xmonad/xmonad.hs
+++ config/xmonad/xmonad.hs
@@ -6,10 +6,19 @@ import XMonad.Actions.Commands
 import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
 
+import qualified XMonad.StackSet as W
+import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
 
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Spacing
+
+mySB = statusBarProp "xmobar ~/.config/xmobar/xmobarrc" (pure xmobarPP)
 main :: IO ()
-main = xmonad $ myConf
+main = xmonad . ewmhFullscreen . ewmh . docks . withSB mySB $ myConf
 
 myConf = def
     { modMask            = mod4Mask
@@ -18,15 +27,22 @@ myConf = def
     , borderWidth        = 1
     , normalBorderColor  = "#000000"
     , focusedBorderColor = "#cccccc"
+    , layoutHook         = myLayoutHook
+    , manageHook         = myManageHook
     }
     `additionalKeysP`
     [ ("M-S-e", spawn "emacsclient -c -n -a 'emacs'")
     , ("M-<Space>", spawn "rofi -modi windowcd,run -show combi --combi-modi windowcd,drun")
     , ("M-<Tab>", sendMessage NextLayout)
     , ("M-S-;", commands >>= runCommand)
-        -- Banish or beckon cursor, akin to Stump
+      -- Banish or beckon cursor, akin to Stump
     , ("M-S-b", banishScreen LowerRight)
     , ("M-b", warpToWindow 0.5 0.5)
+      -- Gaps
+    , ("M-g", toggleWindowSpacingEnabled)
+    , ("M-[", decScreenWindowSpacing 2)
+    , ("M-]", incScreenWindowSpacing 2)
+      -- volume
     , ("<XF86AudioLowerVolume>", spawn "pamixer -d 4; vol-info")
     , ("<XF86AudioRaiseVolume>", spawn "pamixer -i 4; vol-info")
     , ("<XF86AudioMute>", spawn "pamixer -t; vol-info")
@@ -34,3 +50,23 @@ myConf = def
 
 commands :: X [(String, X())]
 commands = defaultCommands
+
+myLayoutHook = smartBorders $ avoidStruts $
+               spacingWithEdge 3 (myTall |||
+                                   Mirror (myTall)) |||
+               Full
+               where
+                 myTall  = Tall nmaster delta ratio
+                 nmaster = 1
+                 delta   = 3/100
+                 ratio   = 1/2
+
+-- If adding more in the future:
+-- myManageHook = (otherStuff) <+> (fmap not isDialog --> doF avoidMaster)
+myManageHook = fmap not isDialog --> doF avoidMaster
+
+-- Windows do not displace master window when it is focused
+avoidMaster :: W.StackSet i l a s sd -> W.StackSet i l a s sd
+avoidMaster = W.modify' $ \c -> case c of
+     W.Stack t [] (r:rs) ->  W.Stack t [r] rs
+     otherwise           -> c
